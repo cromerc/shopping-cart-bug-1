7a362b6a8f2da835e4845dd0c317a20957f0cb74
Chris Cromer <chris@cromer.cl>
2023-06-02 16:06:14 -0400

    finish challenge

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,9 @@ const products = ref([
 ]);
 
 const total = computed(() => {
-  return Math.round(
-    products.value.reduce((acc, product) => acc + product.price, 0) / 100
-  );
+  return (
+    products.value.reduce((acc, product) => acc + product.price * product.quantity, 0) / 100
+  ).toFixed(2);
 });
 
 function increaseQuantity(product) {
@@ -23,43 +23,34 @@ function increaseQuantity(product) {
 }
 
 function decreaseQuantity(product) {
+  if (product.quantity == 0) {
+    return;
+  }
   product.quantity -= 1;
 }
 </script>
 <template>
-  <div
-    class="flex items-center justify-center dark:text-white h-screen w-screen"
-  >
-    <div
-      class="max-w-3xl w-full border border-gray-50 dark:border-gray-600 shadow-2xl rounded-md"
-    >
+  <div class="flex items-center justify-center dark:text-white h-screen w-screen">
+    <div class="max-w-3xl w-full border border-gray-50 dark:border-gray-600 shadow-2xl rounded-md">
       <h2 class="text-4xl bg-gray-600 p-8 text-gray-200">Shopping Cart</h2>
       <ul class="dark:text-white p-8">
-        <li
-          v-for="product in products"
-          :key="product.id"
-          class="flex flex-col my-2 w-full odd:bg-gray-100 odd:dark:bg-gray-700 p-4"
-        >
+        <li v-for="product in products" :key="product.id"
+          class="flex flex-col my-2 w-full odd:bg-gray-100 odd:dark:bg-gray-700 p-4">
           <span class="flex items-center justify-between w-full space-x-3">
             <span class="w-1/3 truncate">{{ product.name }}</span>
-            <span>${{ product.price / 100 }}</span>
+            <span>${{ (product.price / 100).toFixed(2) }}</span>
             <span class="inline-flex items-center justify-between space-x-3">
-              <button
-                class="bg-blue-400 hover:bg-blue-600 py-1 px-2 rounded-md"
-                @click="decreaseQuantity(product)"
-              >
+              <button :disabled="product.quantity <= 0" class="bg-blue-400 hover:bg-blue-600 py-1 px-2 rounded-md"
+                @click="decreaseQuantity(product)">
                 -
               </button>
               <span class="w-12 text-center">{{ product.quantity }}</span>
-              <button
-                class="bg-blue-400 hover:bg-blue-600 py-1 px-2 rounded-md"
-                @click="increaseQuantity(product)"
-              >
+              <button class="bg-blue-400 hover:bg-blue-600 py-1 px-2 rounded-md" @click="increaseQuantity(product)">
                 +
               </button>
             </span>
             <span class="w-16 text-right">
-              ${{ (product.price * product.quantity) / 100 }}
+              ${{ ((product.price * product.quantity) / 100).toFixed(2) }}
             </span>
           </span>
         </li>
